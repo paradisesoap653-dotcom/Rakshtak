56655f02144f3aeb488ee5714ce50acb41feb0fe
paradisesoap653-dotcom <paradisesoap653@gmail.com>
2026-07-25 15:49:19 +0000

    تحديث المكتبات إلى الإصدارات المتوافقة

--- .next/types/validator.ts
+++ .next/types/validator.ts
@@ -92,6 +92,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
+// Validate ../../src/app/api/admin/stats/route.ts
+{
+  type __IsExpected<Specific extends RouteHandlerConfig<"/api/admin/stats">> = Specific
+  const handler = {} as typeof import("../../src/app/api/admin/stats/route.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../src/app/api/auth/login/route.ts
 {
   type __IsExpected<Specific extends RouteHandlerConfig<"/api/auth/login">> = Specific
